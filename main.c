e21bcb3099018ab3d01b745f38e5659781240f8a
Matalya <matalya8@gmail.com>
2023-10-04 21:05:09 -0300

    Finished untested version.

--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 
 //Contains a head and tail int, a size_t length
 //
-//and a pointer to a (heap?) array.
+//and a pointer to a (heap-assumed) array.
 typedef struct {
     int* array;
     int head;
     int tail;
+    size_t capacity;
     size_t length;
 } Circle;
 
@@ -15,11 +16,11 @@ typedef struct {
 //
 //been added until the tail came right by the head.
 int is_circ_full(Circle* circle) {
-    return circle->head == (circle->tail + 1) % circle->length;
+    return circle->head == (circle->tail + 1) % circle->capacity;
 }
 
-//All circles are initialized with head and tail = 0.
-int circ_is_empty(Circle* circle) {
+//All empty circles are initialized with head and tail = -1.
+int is_circ_empty(Circle* circle) {
     return circle->head == -1;
 }
 
@@ -28,23 +29,24 @@ Circle* initCircle(size_t size) {
     Circle* circle = malloc(sizeof(Circle));
     circle->array = malloc(sizeof(int) * size);
     circle->head = circle->tail = -1;
-    circle->length = size;
+    circle->capacity = size;
+    circle->length = 0;
     return circle;
 }
 
 //Specialized function that doubles the size of the circle.
-Circle* circleIsFull(Circle* circle) {
-    Circle* newCircle = initCircle(circle->length * 2);
+Circle* doubleCircleCapacity(Circle* circle) {
+    Circle* newCircle = initCircle(circle->capacity * 2);
     int copyFrom = circle->head;
     int copyTo = -1;
 
     do {
         newCircle->array[copyTo++] = circle->head;
-        copyFrom = (copyFrom + 1) % circle->length;
+        copyFrom = (copyFrom + 1) % circle->capacity;
     } while (copyFrom != circle->head);
 
     newCircle->head = 0;
-    newCircle->tail = circle->length - 1;
+    newCircle->tail = circle->capacity - 1;
     return newCircle;
 }
 
@@ -57,6 +59,7 @@ void nukeCircle(Circle* circle) {
     free(circle->array);
     circle->array = malloc(sizeof(int));
     circle->head = circle->tail = -1;
+    circle->length = 0;
 }
 
 //Restarts the function by setting head and tail to -1
@@ -66,32 +69,66 @@ void nukeCircle(Circle* circle) {
 //overwrite it.
 void emptyCircle(Circle* circle) {
     circle->head = circle->tail = -1;
+    circle->length = 0;
 }
 
-//Adds an item according to the FIFO principle
-void enqueueItem(Circle* circle, int value) {
+//Adds an item according to the FIFO principle.
+//
+//If full, circle is doubled in size.
+void enqueueItemSafe(Circle* circle, int value) {
     if (is_circ_full(circle)) {
-        circle = circleIsFull(circle);
-    } else if (circ_is_empty(circle)) {
+        circle = doubleCircleCapacity(circle);
+    } else if (is_circ_empty(circle)) {
         circle->head++;
     }
-    circle->tail = (circle->tail + 1) % circle->length;
+    circle->tail = (circle->tail + 1) % circle->capacity;
     circle->array[circle->tail] = value;
+    circle->length++;
 }
+
+//Adds an item according to the FIFO principle.
+//
+//If full, it'll simply overwrite the last item.
+void enqueueItem(Circle* circle, int value) {
+    if (is_circ_full(circle) || is_circ_empty(circle)) {
+        circle->head++;
+    }
+    circle->tail = (circle->tail + 1) % circle->capacity;
+    circle->array[circle->tail] = value;
+    circle->length++;
+}
+
+//Returns the first element of the circle. Contains no
+//
+//error handling.
+int peekCircle(Circle* circle) {
+    return circle->array[circle->head];
+}
+
 
 //Eliminates an item according to the FIFO
 //
 //principle. Returns a 0 if failure, 1 if success.
 int dequeueItem(Circle* circle) {
-    if (circ_is_empty(circle)) {
+    if (is_circ_empty(circle)) {
         return 0;
     }
     if (circle->head == circle->tail) {
         emptyCircle(circle);
     } else {
-        circle->head = (circle->head + 1) % circle->length;
+        circle->head = (circle->head + 1) % circle->capacity;
+        circle->length--;
     }
     return 1;
+}
+//Sister function to dequeue item. It returns the dequeued
+//
+//item. Has no error handling cuz I can't be bothered anymore.
+int ripLastItemOff(Circle* circle) {
+    int item = circle->head;
+    circle->head = (circle->head + 1) % circle->capacity;
+    circle->length--;
+    return item;
 }
 
 int main() {
